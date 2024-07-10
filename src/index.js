e70cebc4048a26cf73eee3ae880a9273b825f517
Had1na <gadubas@gmail.com>
2024-07-10 15:05:54 +0100

    Added search functionality using the API

--- src/index.js
+++ src/index.js
@@ -1,10 +1,24 @@
+function refreshWeather(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = response.data.temperature.current;
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = Math.round(temperature);
+}
+
+function searchCity(city) {
+  let apiKey = "137ae6afd69a1ab4tc7ba7202of80b98";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(refreshWeather);
+}
+
 function searchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
-  let cityElement = document.querySelector("#city");
-  cityElement.innerHTML = searchInput.value;
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
-
 searchFormElement.addEventListener("submit", searchSubmit);
+
+searchCity("London");
